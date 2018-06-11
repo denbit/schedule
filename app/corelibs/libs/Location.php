af9447966ff3f67c7d0c674ca6c98c630e577097
DenysBarsuk <den_b_it@i.ua>
2018-06-11 11:44:00 +0300

    new

---
+++ app/corelibs/libs/Location.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: DenysBarsuk
+ * Date: 11.06.2018
+ * Time: 11:24
+ */
+
+namespace Schedule\Core;
+
+
+class Location
+{
+
+}
---
+++ app/corelibs/models/Cities.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: DenysBarsuk
+ * Date: 11.06.2018
+ * Time: 11:11
+ */
+
+namespace CoreLibs\Models;
+
+
+class Cities
+{
+
+}
