bbedfdf93f0632194298bddb88d484840b5adb35
Administrator <den_b_it@i.ua>
2018-10-03 16:02:35 +0300

    fix

---
+++ app/corelibs/libs/Kernel.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: DenysBarsuk
+ * Date: 03.10.2018
+ * Time: 15:21
+ */
+
+namespace Schedule\Core;
+
+
+/**
+ * Phalcon\Di\Injectable
+ *
+ * This class allows to access services in the services container by just only accessing a public property
+ * with the same name of a registered service
+ *
+ * @property \Phalcon\Mvc\Dispatcher|\Phalcon\Mvc\DispatcherInterface $dispatcher
+ * @property \Phalcon\Mvc\Router|\Phalcon\Mvc\RouterInterface $router
+ * @property \Phalcon\Mvc\Url|\Phalcon\Mvc\UrlInterface $url
+ * @property \Phalcon\Http\Request|\Phalcon\Http\RequestInterface $request
+ * @property \Phalcon\Http\Response|\Phalcon\Http\ResponseInterface $response
+ * @property \Phalcon\Http\Response\Cookies|\Phalcon\Http\Response\CookiesInterface $cookies
+ * @property \Phalcon\Filter|\Phalcon\FilterInterface $filter
+ * @property \Phalcon\Flash\Direct $flash
+ * @property \Phalcon\Flash\Session $flashSession
+ * @property \Phalcon\Session\Adapter\Files|\Phalcon\Session\Adapter|\Phalcon\Session\AdapterInterface $session
+ * @property \Phalcon\Events\Manager|\Phalcon\Events\ManagerInterface $eventsManager
+ * @property \Phalcon\Db\AdapterInterface $db
+ * @property \Phalcon\Security $security
+ * @property \Phalcon\Crypt|\Phalcon\CryptInterface $crypt
+ * @property \Phalcon\Tag $tag
+ * @property \Phalcon\Escaper|\Phalcon\EscaperInterface $escaper
+ * @property \Phalcon\Annotations\Adapter\Memory|\Phalcon\Annotations\Adapter $annotations
+ * @property \Phalcon\Mvc\Model\Manager|\Phalcon\Mvc\Model\ManagerInterface $modelsManager
+ * @property \Phalcon\Mvc\Model\MetaData\Memory|\Phalcon\Mvc\Model\MetadataInterface $modelsMetadata
+ * @property \Phalcon\Mvc\Model\Transaction\Manager|\Phalcon\Mvc\Model\Transaction\ManagerInterface $transactionManager
+ * @property \Phalcon\Assets\Manager $assets
+ * @property \Phalcon\Di|\Phalcon\DiInterface $di
+ * @property \Phalcon\Session\Bag|\Phalcon\Session\BagInterface $persistent
+ * @property \Phalcon\Mvc\View|\Phalcon\Mvc\ViewInterface $view
+ */
+class Kernel
+{
+    /**
+     * @var \Phalcon\Di
+     */
+private static $di_inst=null;
+    /**
+     * @var \Phalcon\Di
+     */
+protected $di;
+
+public function __construct()
+{
+    if(is_null(self::$di_inst)){
+        global $application;
+        $this->di=self::$di_inst=$application->getDI();
+    }else
+    $this->di=self::$di_inst;
+
+}
+public function  __get($var){
+    if(property_exists($this->di,$var))
+        return $this->di[$var];
+    else
+        return false;
+}
+}
