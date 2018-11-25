490ea6f97fdcb83b3476ebbbd3f0c1c725eb1e4e
denbit <den_b_it@i.ua>
2018-11-25 14:52:53 +0200

    hbfj

---
+++ app/modules/transporters/controllers/CompanyController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: DenysBarsuk
+ * Date: 25.11.2018
+ * Time: 12:06
+ */
+
+namespace Schedule\Modules\Transporters\Controllers;
+
+
+
+class CompanyController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+        return "helo";
+    }
+}
