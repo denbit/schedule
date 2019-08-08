c4f60b156a0186203ccbe6e3d48fa74b3df6e8b3
ElikaFlin <elika.filin@gmail.com>
2019-08-08 21:10:59 +0300

    added tree

--- app/modules/authority/models/LocationManager.php
+++ app/modules/authority/models/LocationManager.php
@@ -13,7 +13,7 @@ use Schedule\Core\Models\Stations;
 class LocationManager
 {
 
-	public function getOverview()
+	public function getOverview(): object
 	{
 		$list = [
 			'countries' => States::count(),
@@ -28,8 +28,37 @@ class LocationManager
 		$result_set = [
 			'list' => $list,
 			'per_state' => $per_state_tree,
+			'tree_list' => $this->build_list_tree($per_state_tree),
 		];
 
-		return $result_set;
+		return (object)$result_set;
+	}
+
+	private function build_list_tree($tree)
+	{
+		$string = '';
+		$closere = function ($item, $key, $closere) use (&$string) {
+			$string .= $this->get_template($key, $item);
+			if (is_array($item) && is_array($item['children'])) {
+				array_walk($item['children'], $closere, $closere);
+			}
+			$string .= "</ul></li>";
+		};
+
+		array_walk($tree, $closere, $closere);
+
+		return $string;
+
+	}
+
+	private function get_template($key, $item)
+	{
+		return <<< HTML
+<li data-id=$key> 
+	<button class="btn btn-link collapsed" data-toggle="collapse" href="#{$item['name']}_collapse" role="button" aria-expanded="false" aria-controls="collapse">
+	{$item['name']}
+	</button>
+	<ul class="collapse" id="{$item['name']}_collapse">
+HTML;
 	}
 }
