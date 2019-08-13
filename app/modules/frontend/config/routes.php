d2ef6897c0dc24bcdbb00481b0502f8784db355b
denbitv2 <denys.b@cudev.org>
2019-08-13 18:35:03 +0300

    related delate

--- app/modules/frontend/config/routes.php
+++ app/modules/frontend/config/routes.php
@@ -6,49 +6,63 @@
  * Time: 15:26
  */
 
-$frontend = new \Phalcon\Mvc\Router\Group([
-	'module'    => 'frontend' ,
-	'namespace' => $namespaces['frontend'] ,
-]);
+$frontend = new \Phalcon\Mvc\Router\Group(
+	[
+		'module' => 'frontend',
+		'namespace' => $namespaces['frontend'],
+	]
+);
 
-$frontend->add('/' , [
+$frontend->add(
+	'/',
+	[
 
-	'controller' => 'index' ,
-	'action'     => 'index' ,
+		'controller' => 'index',
+		'action' => 'index',
 
-]);
-$frontend->add('/:controller/' , [
-	'controller' => 1 ,
-	'action'     => 'index' ,
+	]
+);
+$frontend->add(
+	'/:controller/',
+	[
+		'controller' => 1,
+		'action' => 'index',
 
-]);
-$frontend->add('/:action' , [
+	]
+);
+$frontend->add(
+	'/:action',
+	[
 
-	'controller' => 'index' ,
-	'action'     => 1 ,
+		'controller' => 'index',
+		'action' => 1,
 
-]);
-$frontend->add('/:controller/:action(/?)' , [
-	'controller' => 1 ,
-	'action'     => 2,
+	]
+);
+$frontend->add(
+	'/:controller/:action(/?)',
+	[
+		'controller' => 1,
+		'action' => 2,
 
-]);
-$dinamic_router = \Schedule\Core\Models\UniversalPage::find([
-	' has_permanent_uri =0',
-	'columns'=>'url,module_name',
-	'group'=>'module_name',
-]);
-try
-{
+	]
+);
+$dinamic_router = \Schedule\Core\Models\UniversalPage::find(
+	[
+		' has_permanent_uri =0',
+		'columns' => 'url,module_name',
+		'group' => 'module_name',
+	]
+);
+try {
 	/**
 	 * @var \Schedule\Core\Models\UniversalPage $dynamic_route
 	 */
-	foreach ($dinamic_router as $dynamic_route ){
-		$frontend->addGet($dynamic_route->readAttribute('url'),$dynamic_route->readAttribute('module_name'));
+	foreach ($dinamic_router as $dynamic_route) {
+		$frontend->addGet($dynamic_route->readAttribute('url'), $dynamic_route->readAttribute('module_name'));
 	}
-} catch (Exception $exception){
-	echo  $exception->getMessage();
+} catch (Exception $exception) {
+	echo $exception->getMessage();
 }
-var_dump($frontend);
 $router->mount($frontend);
 
