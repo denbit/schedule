2f75c8a20209643bfd1c5b6256c7f345e23a5d04
denbit <den_b_it@i.ua>
2020-01-30 15:54:17 +0200

    added toolbox for starting

---
+++ public/webtools.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Developer Tools.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+use Phalcon\DevTools\Bootstrap;
+error_reporting(E_ALL);
+ini_set('display_errors','on');
+
+/**
+ * @psalm-suppress MissingFile
+ */
+include 'webtools.config.php';
+include PTOOLSPATH . '/bootstrap/autoload.php';
+
+/**
+ * @psalm-suppress UndefinedConstant
+ */
+$bootstrap = new Bootstrap([
+    'ptools_path' => PTOOLSPATH,
+    'ptools_ip'   => PTOOLS_IP,
+    'base_path'   => BASE_PATH,
+]);
+
+if (APPLICATION_ENV === ENV_TESTING) {
+    return $bootstrap->run();
+} else {
+    echo $bootstrap->run();
+}
